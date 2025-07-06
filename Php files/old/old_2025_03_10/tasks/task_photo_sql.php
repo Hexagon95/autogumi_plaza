84e6cd667a2df4d7354d8f3a2bfeaf7b75fd96bf
Hexagon95 <vazul95@gmail.com>
2025-07-06 20:13:53 +0200

    newest version

---
+++ Php files/old/old_2025_03_10/tasks/task_photo_sql.php
@@ -0,0 +1,66 @@
+<?php
+class Task {
+    // ---------- <Variables [1]> ----- ---------- ---------- ---------- ---------- ---------- ---------- ----------    
+    private $sqlCommand;
+    private $sqlQuery;
+    private $databaseManager;
+    private $request;
+    private $BizonylatDokumentumParams;
+    private $Customer;
+    private $result;
+        public function getResult(){return $this->result;
+    }
+
+    // ---------- <Constructors> ------ ---------- ---------- ---------- ---------- ---------- ---------- ----------
+    function __construct($BizonylatDokumentumParams, $Customer){
+        $this->BizonylatDokumentumParams = $BizonylatDokumentumParams;
+        $this->Customer = $Customer;
+        $this->_inizialite();        
+    }
+
+    // ---------- <Methods [1]> ------- ---------- ---------- ---------- ---------- ---------- ---------- ----------
+     private function _inizialite() {
+        $this->request = json_encode($this->BizonylatDokumentumParams, JSON_PRETTY_PRINT);
+        $this->sqlCommand =         new SqlCommand();
+        $this->databaseManager =    new DatabaseManager(
+            $this->sqlCommand->exec_bizonylatKepFelvitele(),
+            ['parameter' => $this->request],
+            $this->Customer
+        );
+        $this->result = $this->databaseManager->getData();
+    }
+}
+
+class Task2 {
+    // ---------- <Variables [1]> ----- ---------- ---------- ---------- ---------- ---------- ---------- ----------
+    private $sqlCommand;
+    private $sqlQuery;
+    private $databaseManager;
+    private $request;
+    private $ParentID;
+    private $Customer;
+    private $result;
+    public function getResult(){return $this->result;
+    }
+
+    // ---------- <Constructors> ------ ---------- ---------- ---------- ---------- ---------- ---------- ----------
+    function __construct($ParentID, $Customer){
+        $this->ParentID = $ParentID;
+        $this->Customer = $Customer;
+        $this->_inizialite();
+    }
+
+    // ---------- <Methods [1]> ------- ---------- ---------- ---------- ---------- ---------- ---------- ----------
+    private function _inizialite() {
+        $this->request = $this->ParentID;
+        $this->sqlCommand =         new SqlCommand();
+		
+        $this->databaseManager =    new DatabaseManager(
+            $this->sqlCommand->exec_bizonylatID_SQL(),
+            ['id' => $this->request],
+            $this->Customer
+        );
+        $this->result = $this->databaseManager->getData();
+    }
+}
+
